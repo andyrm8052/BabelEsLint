0b280cd576661587b3bcd35a5e0393758e128406
andyrm8052 <andy1994>
2021-04-24 16:44:43 -0400

    Commit: Express Authentication

--- server/app.js
+++ server/app.js
@@ -1,24 +1,155 @@
-'use strict';
-const express = require('express');
-const app = express();
-const open = require("open");
+//'use strict';
+const express = require('express')
+const exphbs = require('express-handlebars');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-let cors = require('cors')
-app.use(cors())
-app.use(express.static('docs'));
+const app = express();
+const crypto = require('crypto');
 
-const port = process.env.PORT || 5000;
-app.use(bodyParser.urlencoded({ extended: true}))
-app.use(bodyParser.json());
+const getHashedPassword = (password) => {
+    const sha256 = crypto.createHash('sha256');
+    const hash = sha256.update(password).digest('base64');
+    return hash;
+}
 
-const citiesRoutes = require('./routes/cities.routes');
-app.use('/api/v1/cities', citiesRoutes);
+const users = [
+    // This user is added to the array to avoid creating a new user on each restart
+    {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'johndoe@email.com',
+        // This is the SHA256 hash for value of `password`
+        password: 'XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg='
+    }
+];
 
-app.set('port', process.env.PORT || 8000);
-app.set('ip', process.env.NODEJS_IP || '127.0.0.1');
+app.post('/register', (req, res) => {
+    const { email, firstName, lastName, password, confirmPassword } = req.body;
 
-app.listen(app.get('port'), function() {
-    console.log('%s: Node server started on %s ...', Date(Date.now()), app.get('port'));
-    open("http://localhost:8000");
+    // Check if the password and confirm password fields match
+    if (password === confirmPassword) {
 
+        // Check if user with the same email is also registered
+        if (users.find(user => user.email === email)) {
+
+            res.render('register', {
+                message: 'User already registered.',
+                messageClass: 'alert-danger'
+            });
+
+            return;
+        }
+
+        const hashedPassword = getHashedPassword(password);
+
+        // Store user into the database if you are using one
+        users.push({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword
+        });
+
+        res.render('login', {
+            message: 'Registration Complete. Please login to continue.',
+            messageClass: 'alert-success'
+        });
+    } else {
+        res.render('register', {
+            message: 'Password does not match.',
+            messageClass: 'alert-danger'
+        });
+    }
+});
+
+const generateAuthToken = () => {
+    return crypto.randomBytes(30).toString('hex');
+}
+
+const authTokens = {};
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    const hashedPassword = getHashedPassword(password);
+
+    const user = users.find(u => {
+        return u.email === email && hashedPassword === u.password
+    });
+
+    if (user) {
+        const authToken = generateAuthToken();
+
+        // Store authentication token
+        authTokens[authToken] = user;
+
+        // Setting the auth token in cookies
+        res.cookie('AuthToken', authToken);
+
+        // Redirect user to the protected page
+        res.redirect('/protected');
+    } else {
+        res.render('login', {
+            message: 'Invalid username or password',
+            messageClass: 'alert-danger'
+        });
+    }
+});
+
+const requireAuth = (req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        res.render('login', {
+            message: 'Please login to continue',
+            messageClass: 'alert-danger'
+        });
+    }
+};
+
+app.get('/protected', requireAuth, (req, res) => {
+    res.render('protected');
+});
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(cookieParser());
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine','hbs');
+app.listen(3000);
+
+//request handle for the home page
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+//request handle for registration
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+//request handle for login page
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.use((req, res, next) => {
+    // Get auth token from the cookies
+    const authToken = req.cookies['AuthToken'];
+
+    // Inject the user to the request
+    req.user = authTokens[authToken];
+    next();
+});
+
+//request handle for protected page
+app.get('/protected', (req, res) => {
+    if (req.user) {
+        res.render('protected');
+    } else {
+        res.render('login', {
+            message: 'Please login to continue',
+            messageClass: 'alert-danger'
+        });
+    }
 });
