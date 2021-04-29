de0b5a67b229c914d443d6ad48f9a130ce72e444
andyrm8052 <andy1994>
2021-04-28 23:31:52 -0400

    Commit: Configuring Passport Configure passport with the application  settings

--- server/app.js
+++ server/app.js
@@ -45,6 +45,43 @@ if (app.get("env") === "production") {
     session.cookie.secure = true;
 }
 
+/**
+ * Passport Configuration
+ */
+
+const strategy = new Auth0Strategy(
+    {
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        callbackURL: process.env.AUTH0_CALLBACK_URL
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+        /**
+         * Access tokens are used to authorize users to an API
+         * (resource server)
+         * accessToken is the token to call the Auth0 API
+         * or a secured third-party API
+         * extraParams.id_token has the JSON Web Token
+         * profile has all the information from the user
+         */
+        return done(null, profile);
+    }
+);
+
+/**
+ *  App Configuration
+ */
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(expressSession(session));
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
 const users = [
     // This user is added to the array to avoid creating a new user on each restart
     {
@@ -189,7 +226,9 @@ app.get('/protected', (req, res) => {
     }
 });
 
-//auth0
+
+
+//auth0 configuration from Node.js Quickstart
 const config = {
     authRequired: false,
     auth0Logout: true,
