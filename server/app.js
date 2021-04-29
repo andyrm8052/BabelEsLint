b0e35cc6de70262566e021b55b891d43cb340a55
andyrm8052 <andy1994>
2021-04-28 23:12:03 -0400

    Commit: Node.js Quickstart Get User Profile Info

--- server/app.js
+++ server/app.js
@@ -186,3 +186,9 @@ app.get('/', (req, res) => {
     res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
 
+//Getting user profile information
+const { requiresAuth } = require('express-openid-connect');
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+});
