67ffd58a1b54b5b34b8a4a413356899ec0abe058
andyrm8052 <andy1994>
2021-05-04 11:14:48 -0400

    Commit: Express Middleware to books.js Added configuration for handling authentication for Express middleware

--- server/books.js
+++ server/books.js
@@ -9,3 +9,63 @@ app.use(bodyParser.json());
 app.listen(4000, () => {
     console.log('Books service started on port 4000');
 });
+
+
+const books = [
+    {
+        "author": "Chinua Achebe",
+        "country": "Nigeria",
+        "language": "English",
+        "pages": 209,
+        "title": "Things Fall Apart",
+        "year": 1958
+    },
+    {
+        "author": "Hans Christian Andersen",
+        "country": "Denmark",
+        "language": "Danish",
+        "pages": 784,
+        "title": "Fairy tales",
+        "year": 1836
+    },
+    {
+        "author": "Dante Alighieri",
+        "country": "Italy",
+        "language": "Italian",
+        "pages": 928,
+        "title": "The Divine Comedy",
+        "year": 1315
+    },
+];
+
+/**
+* Request Handler for Books
+ **/
+app.get('/books', (req, res) => {
+    res.json(books);
+});
+
+
+/**
+ * Express Middleware to Handle Authentication
+ **/
+const accessTokenSecret = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImpvaG4iLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2MjAxNDA5MzF9.szU_qGXRAPGNIwlVrucaqQh3QI5QCu39UZ6dRU1pAhI";
+
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, accessTokenSecret, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
