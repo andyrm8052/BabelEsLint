29a8f0c7cb77317985e2a060aa94837a91bac732
andyrm8052 <andy1994>
2021-05-04 10:48:55 -0400

    Commit: Added Authentication Service Added configuration for user

---
+++ server/auth.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Authentication service started on port 3000');
+});
+
+const users = [
+    {
+        username: 'john',
+        password: 'password123admin',
+        role: 'admin'
+    }, {
+        username: 'anna',
+        password: 'password123member',
+        role: 'member'
+    }
+];
