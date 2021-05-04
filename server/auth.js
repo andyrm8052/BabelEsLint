e1d9a4547a2545e71d7818c6fbd4d3b20759d535
andyrm8052 <andy1994>
2021-05-04 10:56:03 -0400

    Commit: Added Login Configuration Added configuration for matching login user data

--- server/auth.js
+++ server/auth.js
@@ -34,3 +34,26 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 const accessTokenSecret = 'youraccesstokensecret';
+
+
+/**
+ * Login Configuration for Matching Data
+ **/
+app.post('/login', (req, res) => {
+    // Read username and password from request body
+    const { username, password } = req.body;
+
+    // Filter user from the users array by username and password
+    const user = users.find(u => { return u.username === username && u.password === password });
+
+    if (user) {
+        // Generate an access token
+        const accessToken = jwt.sign({ username: user.username,  role: user.role }, accessTokenSecret);
+
+        res.json({
+            accessToken
+        });
+    } else {
+        res.send('Username or password incorrect');
+    }
+});
